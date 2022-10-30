c0d3e4deded6f3d643c5376040a851f27b433965
Simon <simon@Simons-MacBook-Pro.local>
2022-10-30 18:30:49 +1300

    invoke("text") no needed

--- cypress/support/pages/resultsPage.js
+++ cypress/support/pages/resultsPage.js
@@ -7,9 +7,8 @@ class ResultsPage {
         // RESULTS FOUND?
         this.resultsFound = ()=> cy.get('.tabs-container')
         // PRICE COMPARISON
-        this.cheaperPriceText = ()=> cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1) > cluster.VIRTUAL_INTERLINING_2OW > .eva-3-cluster-basic > .cluster-container > .cluster-pricebox-container > .fare-box-container > .fare-container > .fare-wrapper > main-fare > .fare-price-wrapper > .fare > :nth-child(1) > flights-price > :nth-child(1) > flights-price-element > .price-currency > .flight-price-label > em > .amount')
+        this.cheaperPriceText = ()=> cy.get('.eva-3-cluster-basic > .cluster-container > .cluster-pricebox-container > .fare-box-container > .fare-container > .fare-wrapper > main-fare > .fare-price-wrapper > .fare > :nth-child(1) > flights-price > :nth-child(1) > flights-price-element > .price-currency > .flight-price-label > em > .amount').eq(1)
         this.cheaperPriceNoDatesText = ()=> cy.get(':nth-child(1) > :nth-child(1) > :nth-child(1) > .VIRTUAL_INTERLINING_2RT > .margin-bottom-reduced-cluster > :nth-child(1) > .eva-3-cluster-basic > .cluster-container > .cluster-pricebox-container > .amount > .eva-3-pricebox-cluster > .pricebox-value-container > .pricebox-value > .pricebox-big-text')
-
     }
 
     // ACTIONS
@@ -26,14 +25,14 @@ class ResultsPage {
 
         if(Cypress.env('dates') === 'noDates'){
             this.cheaperPriceNoDatesText()
-            .invoke('text')
+            //.invoke('text')
             .then(number => parseFloat(number))
             .then(number => number*1000)
             .then(number => cy.log('Lower price for ',Cypress.env('from')+' > '+Cypress.env('to')+' '+passengers[0]+' Adults and '+passengers[1]+' Children is : ARS$'+number))
             .should('not.be.gt',Cypress.env('maxPrice'))
         } else {
             this.cheaperPriceText()
-            .invoke('text')
+            //.invoke('text')
             .then(number => parseFloat(number))
             .then(number => number*1000)
             .then(number => cy.log('Lower price for ',Cypress.env('from')+' > '+Cypress.env('to')+' '+passengers[0]+' Adults and '+passengers[1]+' Children is : ARS$'+number))
