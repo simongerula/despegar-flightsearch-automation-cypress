544dcf458f698cbd812e6bfe86070633fd89a611
Simon <simon@Simons-MacBook-Pro.local>
2023-03-12 15:27:48 +1300

    cypress-mochawesome-reporter added

--- cypress.config.js
+++ cypress.config.js
@@ -5,16 +5,24 @@ module.exports = defineConfig({
     typeOfFlight: 'oneWay',
     from: 'EZE',
     to: 'BCN',
-    dates: '2023-01-15',
+    dates: '2023-05-14',
     passengers: '2 1',
     childsAges: '5',
     flightClass: 'Económica',
     maxPrice: 2000000,
   },
   defaultCommandTimeout: 20000,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    html: true,
+    json: false,
+    inlineAssets: true,
+    saveAllAttempts: false
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      require('./cypress/plugins/index.js')(on, config)
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-  },
+  }
 })
